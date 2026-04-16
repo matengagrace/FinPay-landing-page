dbb200fb6b0c71da31d8f8977ef0130ebba70a81
matengagrace <matengagraceandre@gmail.com>
2026-04-16 17:45:59 +0200

    adaptation du css

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import Mission from "./components/Mission";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 
+
 function App() {
+  
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className=" bg-primary w-[100%] fixed top-0 z-[5]">
@@ -23,25 +25,25 @@ function App() {
 
       <div className="w-full bg-gray-50 relative h-[1200px] sm:h-[600px]">
         <div className="w-full bg-primary h-[200px]"></div>
-        <div className="w-[90%] sm:h-[500px] flex justify-center items-center mx-auto mt-15 bg-white rounded-[10px] shadow-lg absolute top-0 left-[5%]">
+        <div id="products"  className=" w-[90%] sm:h-[500px] flex justify-center items-center mx-auto mt-15 bg-white rounded-[10px] shadow-lg absolute top-0 left-[5%]">
           <Princing />
         </div>
       </div>
 
       <div className="w-full bg-gray-50 flex justify-center items-center">
-        <div className="w-[80%] flex justify-center items-center mx-auto bg-gray-50 rounded-[10px] py-5">
+        <div id="customers" className="w-[80%] flex justify-center items-center mx-auto bg-gray-50 rounded-[10px] py-5">
           <Experiences />
         </div>
       </div>
 
       <div className="w-full bg-blueFoncee py-5">
-        <div className="w-[90%] flex justify-center items-center mx-auto mt-15">
+        <div id="pricing" className="w-[90%] flex justify-center items-center mx-auto mt-15">
           <Step />
         </div>
       </div>
 
       <div className="w-full bg-gray-50 flex justify-center items-center py-6">
-        <div className="w-[80%] flex justify-center items-center mx-auto  bg-white rounded-[10px]">
+        <div id="learn" className="w-[80%] flex justify-center items-center mx-auto  bg-white rounded-[10px]">
           <Mission/>
         </div>
       </div>
