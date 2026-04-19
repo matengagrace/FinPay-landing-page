3784a43212590bcdacb878e8b08599df327d024f
matengagrace <matengagraceandre@gmail.com>
2026-04-20 00:30:54 +0200

    modification de css

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -6,7 +6,7 @@ function Experiences() {
 const {elementRef, isVisible} = useIntersectionObserver({threshold:0.2,});
 
   return (
-    <section className="bg-gray-50 w-full justify-center h-[100%] pb-6">
+    <section className="bg-gray-50 w-full justify-center  pb-6">
       <div className="justify-center items-center flex-col flex ">
         <h4 className={`text-blueClaire uppercase font-bold`}>Why us</h4>
         <h2 ref={elementRef} className={` reveal reveal-bottom ${isVisible ? "show" : ""} delay-0 text-[30px] sm:text-[45px] font-bold text-blackClaire`}>
