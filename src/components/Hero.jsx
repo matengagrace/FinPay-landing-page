2af32fc73b0435f05fad6a6a69ccc39ec10566c1
matengagrace <matengagraceandre@gmail.com>
2026-04-22 23:43:04 +0200

    ajout de mises a jour de style tailwindcss dans la partie hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className="w-full flex justify-between items-center md:flex-row flex-col sm:py-2 mt-9 py-6">
       <div className="flex flex-col items-start pr-5 font-bold">
-        <h2 className={`animation-fade-up text-[40px] delay-150 sm:text-[60px] font-bold  text-blackClaire leading-[1.5] sm:leading-[1.1]`}>
+        <h2 className={`animation-fade-up text-[35px] delay-150 sm:text-[60px] font-bold  text-blackClaire leading-[1.5] sm:leading-[1.1]`}>
           Get paid early <br className="sm:block hidden" />
           save automatically <br className="sm:block hidden" />
           all your pay.
@@ -16,7 +16,7 @@ function Hero() {
           and cash flow management tools.
         </p>
 
-        <div  className={`animation-fade-up delay-1000 flex w-full mt-0 sm:mt-5 sm:w-[90%] justify-between items-center flex-col sm:flex-row  relatve sm:border border-gray-400 rounded-[10px]`}>
+        <div  className={`animation-fade-up delay-1000 flex w-full mt-5 sm:w-[90%] justify-between items-center flex-col sm:flex-row  relatve sm:border border-gray-400 rounded-[10px]`}>
           <input type="text" placeholder="Your business message" className=" bg-white border h-[48px] pl-5 mb-[60px] sm:mb-0 border-gray-400 sm:w-[70%] w-full text-gray-400 rounded-l-[10px] rounded-r-[10px] sm:rounded-r-0 outline-0" />
           
           <button className="btn-primary text-white text-[16px] bg-blueClaire border-0 flex absolute bottom-0 right-[23%] sm:right-0 justify-center items-center">
