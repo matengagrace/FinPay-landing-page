665a4b64d9206ee210d6c02527dbf9882e535b3b
matengagrace <matengagraceandre@gmail.com>
2026-04-22 23:39:26 +0200

    ajout de mises a jour de style tailwindcss dans la partie hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className="w-full flex justify-between items-center md:flex-row flex-col sm:py-2 mt-9 py-6">
       <div className="flex flex-col items-start pr-5 font-bold">
-        <h2 className={`animation-fade-up text-[40px] delay-150 sm:text-[68px] font-bold  text-blackClaire leading-[1.5] sm:leading-[1.1]`}>
+        <h2 className={`animation-fade-up text-[40px] delay-150 sm:text-[60px] font-bold  text-blackClaire leading-[1.5] sm:leading-[1.1]`}>
           Get paid early <br className="sm:block hidden" />
           save automatically <br className="sm:block hidden" />
           all your pay.
